98215b514e7ea9ad27bb4b3a0a347231aa614594
oleg <oleg.pav.m@gmail.com>
2023-05-06 15:53:15 +0300

    Bots list.

--- skipper-ui/src/app/my-bots/my-bots.component.ts
+++ skipper-ui/src/app/my-bots/my-bots.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 export enum EBotState {
-    NEW = 'NEW',
-    ACTIVE = 'ACTIVE',
-    DISABLED = 'DISABLED',
+    ACTIVE = 'active',
+    BOOT = 'boot',
+    DISABLED = 'disabled',
 }
 
 export enum EBotType {
@@ -11,12 +11,14 @@ export enum EBotType {
     PARAMETRIZED = 'Parametrized bot',
     SEMI_AUTO = 'Semi-auto bot',
     TOOL = 'Tool bot',
+    FOLLOW = 'Follow bot',
 }
 
 export interface BotListElement {
     name: string;
     type: EBotType;
     status: EBotState;
+    onlineTime: string;
     state: string;
 }
 
@@ -25,25 +27,29 @@ const BOTS_LIST_DEMO: Array<BotListElement> = [
         name: 'Super bot',
         type: EBotType.FULL_AUTO,
         status: EBotState.ACTIVE,
+        onlineTime: '0d 01:23',
         state: 'In position',
     },
     {
         name: 'Alpha bot',
         type: EBotType.PARAMETRIZED,
         status: EBotState.DISABLED,
-        state: '-',
+        onlineTime: 'Offline',
+        state: 'Disabled',
     },
     {
         name: 'Beta bot',
         type: EBotType.SEMI_AUTO,
-        status: EBotState.NEW,
-        state: '-',
+        status: EBotState.ACTIVE,
+        onlineTime: '5d 01:23',
+        state: 'Wait',
     },
     {
         name: 'Helper bot',
         type: EBotType.TOOL,
-        status: EBotState.NEW,
-        state: '-',
+        status: EBotState.BOOT,
+        onlineTime: '0d 00:00',
+        state: 'Boot',
     },
 ];
 
