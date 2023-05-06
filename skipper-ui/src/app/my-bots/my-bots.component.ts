60d2500d10cdfc3f942c0e1166060d3fd22d320b
oleg <oleg.pav.m@gmail.com>
2023-05-06 17:43:32 +0300

    Hard stop status.

--- skipper-ui/src/app/my-bots/my-bots.component.ts
+++ skipper-ui/src/app/my-bots/my-bots.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
-export enum EBotState {
+export enum EBotStatus {
     ACTIVE = 'active',
-    BOOT = 'boot',
+    BOOT_IN = 'boot-in',
+    BOOT_OUT = 'boot-out',
     DISABLED = 'disabled',
+    HARD_STOP = 'hard-stop',
 }
 
 export enum EBotType {
@@ -18,7 +20,7 @@ export interface IBotListElement {
     id: string;
     name: string;
     type: EBotType;
-    status: EBotState;
+    status: EBotStatus;
     onlineTime: string;
     state: string;
 }
@@ -28,7 +30,7 @@ const BOTS_LIST_DEMO: Array<IBotListElement> = [
         id: 'aaa',
         name: 'Super bot',
         type: EBotType.FULL_AUTO,
-        status: EBotState.ACTIVE,
+        status: EBotStatus.ACTIVE,
         onlineTime: '0d 01:23',
         state: 'In position',
     },
@@ -36,7 +38,7 @@ const BOTS_LIST_DEMO: Array<IBotListElement> = [
         id: 'bbb',
         name: 'Alpha bot',
         type: EBotType.PARAMETRIZED,
-        status: EBotState.DISABLED,
+        status: EBotStatus.DISABLED,
         onlineTime: 'Offline',
         state: 'Disabled',
     },
@@ -44,7 +46,7 @@ const BOTS_LIST_DEMO: Array<IBotListElement> = [
         id: 'ccc',
         name: 'Beta bot',
         type: EBotType.SEMI_AUTO,
-        status: EBotState.ACTIVE,
+        status: EBotStatus.ACTIVE,
         onlineTime: '5d 01:23',
         state: 'Wait',
     },
@@ -52,7 +54,7 @@ const BOTS_LIST_DEMO: Array<IBotListElement> = [
         id: 'ddd',
         name: 'Helper bot',
         type: EBotType.TOOL,
-        status: EBotState.ACTIVE,
+        status: EBotStatus.ACTIVE,
         onlineTime: '0d 00:00',
         state: 'Wait',
     },
@@ -64,20 +66,28 @@ const BOTS_LIST_DEMO: Array<IBotListElement> = [
     styleUrls: ['./my-bots.component.scss'],
 })
 export class MyBotsComponent {
-    EBotState = EBotState;
+    EBotStatus = EBotStatus;
     dataSource = BOTS_LIST_DEMO;
 
     onStatusClick(bot: IBotListElement): void {
         // TODO -
         alert(bot.name);
 
-        if (bot.status === EBotState.ACTIVE) {
-            this.onStopClick(bot);
-        } else if (bot.status === EBotState.DISABLED) {
-            this.onStartClick(bot);
-        } else {
-            alert('Hard drop?');
-            this.onStopClick(bot);
+        switch (bot.status) {
+            case EBotStatus.ACTIVE:
+                bot.status = EBotStatus.BOOT_OUT;
+                setTimeout(() => (bot.status = EBotStatus.DISABLED), 5000);
+                break;
+            case EBotStatus.BOOT_OUT:
+            case EBotStatus.BOOT_IN:
+                alert('Hard stop?');
+                bot.status = EBotStatus.HARD_STOP;
+                setTimeout(() => (bot.status = EBotStatus.DISABLED), 5000);
+                break;
+            case EBotStatus.DISABLED:
+                bot.status = EBotStatus.BOOT_IN;
+                setTimeout(() => (bot.status = EBotStatus.ACTIVE), 5000);
+                break;
         }
     }
 
@@ -94,19 +104,38 @@ export class MyBotsComponent {
     onStartClick(bot: IBotListElement): void {
         // TODO -
         alert(bot.name);
-        bot.status = EBotState.BOOT;
-        setTimeout(() => (bot.status = EBotState.ACTIVE), 5000);
+        bot.status = EBotStatus.BOOT_IN;
+        setTimeout(() => (bot.status = EBotStatus.ACTIVE), 5000);
     }
 
     onStopClick(bot: IBotListElement): void {
         // TODO -
         alert(bot.name);
-        bot.status = EBotState.BOOT;
-        setTimeout(() => (bot.status = EBotState.DISABLED), 5000);
+        bot.status = EBotStatus.BOOT_OUT;
+        setTimeout(() => (bot.status = EBotStatus.DISABLED), 5000);
     }
 
     onDeleteClick(bot: IBotListElement): void {
         // TODO -
         alert(bot.name);
+
+        switch (bot.status) {
+            case EBotStatus.ACTIVE:
+                alert('Hard stop?');
+                bot.status = EBotStatus.HARD_STOP;
+                setTimeout(() => (bot.status = EBotStatus.DISABLED), 5000);
+                break;
+            case EBotStatus.BOOT_IN:
+            case EBotStatus.BOOT_OUT:
+                alert('Hard stop?');
+                bot.status = EBotStatus.HARD_STOP;
+                setTimeout(() => (bot.status = EBotStatus.DISABLED), 5000);
+                break;
+            case EBotStatus.DISABLED:
+                const index = this.dataSource.indexOf(bot);
+
+                this.dataSource.splice(index, 1);
+                break;
+        }
     }
 }
